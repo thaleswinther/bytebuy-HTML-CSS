ec7148ffba159f5b333ef6bb5fef5b3478709a4c
thales <thaleswcm@estudante.ufscar.br>
2023-12-15 02:07:36 -0300

    fix: adjusting map and contact banner

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('formulario').style.display = 'none';
-    document.getElementById('localizacao').style.display = 'none';
+    document.querySelectorAll('section').forEach(section => {
+        const id = section.getAttribute('id');
+        if (id !== 'banner' && id !== 'productCarousel' && id !== 'footer') {
+            section.style.display = 'none';
+        }
+    });
+
+    document.getElementById('banner').style.display = 'block';
+    document.getElementById('productCarousel').style.display = 'block';
+
     document.querySelectorAll('.nav-link').forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
             document.querySelectorAll('section').forEach(section => {
                 section.style.display = 'none';
             });
-            document.getElementById('banner').style.display = 'none';
+            
+            document.getElementById('banner').style.display = 'block';
+            document.getElementById('productCarousel').style.display = 'block';
+            
             const target = this.getAttribute('href').substring(1); 
             document.getElementById(target).style.display = 'block';
+            
             if (target === 'formulario') {
                 document.getElementById('localizacao').style.display = 'block';
+                document.getElementById('productCarousel').style.display = 'none';
+                document.getElementById('banner').style.display = 'none';
             }
             if (target === 'localizacao') {
                 document.getElementById('formulario').style.display = 'block';
                 document.getElementById('localizacao').scrollIntoView();
-            }
-            if (target === 'produtos') {
-                document.getElementById('banner').style.display = 'block';
+                document.getElementById('productCarousel').style.display = 'none';
+                document.getElementById('banner').style.display = 'none';
             }
         });
     });

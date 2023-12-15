6b339ce985eb41028a892256e7e2059c8d498414
thales <thaleswcm@estudante.ufscar.br>
2023-12-15 03:27:43 -0300

    fix: editing product cards

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('section').forEach(section => {
         const id = section.getAttribute('id');
-        if (id !== 'banner' && id !== 'productCarousel' && id !== 'footer' && id !== 'banners') {
+        if (id !== 'banner' && id !== 'productCarousel' && id !== 'footer' && id !== 'banners' && id != 'departament') {
             section.style.display = 'none';
         }
     });
 
     document.getElementById('banner').style.display = 'block';
     document.getElementById('productCarousel').style.display = 'block';
+    document.getElementById('departament').style.display = 'block';
 
     document.querySelectorAll('.nav-link').forEach(link => {
         link.addEventListener('click', function(event) {
@@ -23,18 +24,21 @@ document.addEventListener('DOMContentLoaded', function () {
             const target = this.getAttribute('href').substring(1); 
             document.getElementById(target).style.display = 'block';
             
-            if (target === 'formulario') {
-                document.getElementById('localizacao').style.display = 'block';
+            if (target === 'formulario' || target === 'localizacao') {
                 document.getElementById('productCarousel').style.display = 'none';
                 document.getElementById('banner').style.display = 'none';
                 document.getElementById('banners').style.display = 'none';
+                document.getElementById('departament').style.display = 'none';
+            } else {
+                document.getElementById('departament').style.display = 'block';
+            }
+            
+            if (target === 'formulario') {
+                document.getElementById('localizacao').style.display = 'block';
             }
             if (target === 'localizacao') {
                 document.getElementById('formulario').style.display = 'block';
                 document.getElementById('localizacao').scrollIntoView();
-                document.getElementById('productCarousel').style.display = 'none';
-                document.getElementById('banner').style.display = 'none';
-                document.getElementById('banners').style.display = 'none';
             }
         });
     });
